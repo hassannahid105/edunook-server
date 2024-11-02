c0020b4969fdf41440d2561f811bb7e2c3b548a9
nahidHassan405 <infosharemail405@gmail.com>
2024-11-02 10:21:53 +0600

    all subimted assignment api create

--- index.js
+++ index.js
@@ -79,6 +79,11 @@ async function run() {
       });
       res.send(result);
     });
+    // ! get all submited assigment
+    app.get("/allsubmited", async (req, res) => {
+      const result = await submittedAssignmentsCollection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
